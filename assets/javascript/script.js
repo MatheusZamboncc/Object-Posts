8156d359e4996d406b3a3d982588be81f8c72b01
matheuscosta <matheuszambon564@gmail.com>
2023-08-07 15:01:22 -0300

    feat:creat edit and remove posts functios

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,47 +1,82 @@
-const posts = [];
+var posts = [];
+let indexPost = -1;
 
-function savePost(){
-    const title = document.getElementById('title').value;
-    const resume  = document.getElementById('resume').value;    
-    const publisher = document.getElementById('publisher').value;
-    const date = document.getElementById('date').value;
+function savePost() {
+    const title = document.getElementById("title").value;
+    const resume = document.getElementById("resume").value;
+    const publisher = document.getElementById("publisher").value;
+    const date = document.getElementById("date").value;
 
-    console.log(title, resume , publisher, date);
+    if (indexPost == -1) {
 
-    if(title && resume && publisher && date){
-    storePost(title, resume, publisher, date)
+        if (title && resume && publisher && date) {
+            storePost(title, resume, publisher, date);
+            cleanFields();
+        }
     }
-
+    else {
+        if (title && resume && publisher && date) {
+            posts [indexPost] = {
+                title: title,
+                resume: resume,
+                publisher: publisher,
+                date: date
+            };
+            showPosts();
+            indexPost = -1;
+            cleanFields();
+        }
+    }
 }
-function storePost(title, resume, publisher, date){
+
+function storePost(title, resume, publisher, date) {
     const post = {
         title,
         resume,
         publisher,
-        date
-};
-posts.push(post);
-showposts();
-
+        date,
+    }
+    posts.push(post);
+    showPosts();
 }
-function showposts(){
-    let showcontent = "";
 
-    posts.forEach((post ,index) => {
-        showcontent += `
-        <div class = "post">
-        <h2>${post.title}</h2> 
-        <p><strong>Resumo:</strong><p>
-        <p><strong>Autor:</strong><p>
-        <p><strong>Data de Publicação:</strong><p>
+function showPosts() {
+    let showContent = "";
+    posts.forEach((post, index) => {
+        showContent += `
+        <div class = "post"> 
+        <h2>${post.title}</h2>
+        <p><strong>Resumo:</strong> ${post.resume} </p>
+        <p><strong>Autor:</strong> ${post.publisher} </p>
+        <p><strong>Data da publicação</strong> ${post.date} </p>
 
-        <button onclick="editpost(${index})">Editar</button>
-        <button onclick="removepost(${index})">Remover</button>
-
-        
+        <button onclick="editPost(${index})">Editar</button>
+        <button onclick="removePost(${index})">Remover</button>
         </div>
-        `
+        `;
     })
-    document.getElementById("list").innerHTML = showcontent
+    document.getElementById("list").innerHTML = showContent
+}
+
+function cleanFields() {
+    document.getElementById("title").value = "";
+    document.getElementById("resume").value = "";
+    document.getElementById("publisher").value = "";
+    document.getElementById("date").value = "";
+}
+
+function editPost(index) {
+    indexPost = index;
+    const post = posts[index];
+
+    document.getElementById("title").value = post.title;
+    document.getElementById("resume").value = post.resume;
+    document.getElementById("publisher").value = post.publisher;
+    document.getElementById("date").value = post.date;
+}
+
+function removePost(index){
+    posts.splice(index, 1);
+    showPosts();
 
 }
